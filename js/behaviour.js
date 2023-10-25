c49bb3e5b021bfa1c48d0f715e5b9997fede9042
gritNgo <dustspeck00@gmail.com>
2023-10-25 15:51:58 +0100

    buttons now work as intended

--- js/behaviour.js
+++ js/behaviour.js
@@ -1,16 +1,16 @@
-let message;                   
 let playerScore = 0;
 let computerScore = 0;
 let computerSelection;
 let playerSelection;
 
 const result = document.getElementById("result");
+// array-like object nodelist
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         playerSelection = button.value;
-
+        console.log(playerSelection);
         playRound(playerSelection, computerSelection);
 
         if (playerScore === 5 || computerScore === 5) {
@@ -36,19 +36,52 @@ function playRound(playerSelection, computerSelection) {
         (computerSelection == "paper" && playerSelection == "rock")
       )) {
         ++computerScore;
+        if (computerScore === 1) {
+            result.textContent = "You lost. Try again."
+        }
+        else if (computerScore === 2) {
+            result.textContent = "You lost. Try again."
+        }
+        else if (computerScore === 3) {
+            result.textContent = "You lost. Try again."
+        }
+        else if (computerScore === 4) {
+            result.textContent = "You lost. Try again."
+        }
+        else {
+            result.textContent = "You lost. Computer wins."
+        }
+      }
+      else {
+        ++playerScore;
+        if (playerScore === 1) {
+            result.textContent = "You win! 4 more."
+        }
+        else if (playerScore === 2) {
+            result.textContent = "You win! 3 more."
+        }
+        else if (playerScore === 3) {
+            result.textContent = "You win! 2 more."
+        }
+        else if (playerScore === 4) {
+            result.textContent = "You win! 1 more."
+        }
+        else {
+            result.textContent = "You win the game!"
+        }
       }
 }   
 
 function declareWinner() {
 
     if (playerScore > computerScore) {
-        console.log(`You win with ${playerScore} wins!`);
+        result.textContent = "You win the game!";
     }
     else if (computerScore > playerScore) {
-        console.log(`Computer wins with ${computerScore} wins.`);
+        result.textContent = "Computer wins the game..."
     }
     else {
-        console.log("The game is a draw");
+        result.textContent = "The game is a tie.";
     }
 }
 
